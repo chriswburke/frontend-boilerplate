793d5a60094a068a6c17f40bc06bbfb2158c6128
Chris W. Burke <chris@chriswburke.com>
2016-08-11 19:25:53 -0400

    Adding webpack task

---
+++ bin/tasks/webpack.js
@@ -0,0 +1,53 @@
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const buildenv = process.argv[ 2 ];
+const config = {
+    dev: {
+        devtool: 'source-map',
+        entry: {
+            path: './src/js/main.js'
+        },
+        output: {
+            path: './build/js/',
+            filename: 'main.js',
+            sourceMapFilename: '[file].map'
+        },
+        module: {
+            loaders: [ {
+                test: /\.js$/,
+                loader: 'babel-loader?presets[]=es2015',
+                exclude: path.resolve( __dirname, 'node_modules' )
+            } ]
+        }
+
+    },
+    production: {
+        entry: {
+            path: './src/js/main.js'
+        },
+        output: {
+            path: './build/js/',
+            filename: 'main.js'
+        },
+        module: {
+            loaders: [ {
+                test: /\.js$/,
+                loader: 'babel-loader?presets[]=es2015',
+                exclude: path.resolve( __dirname, 'node_modules' )
+            } ]
+        }
+
+    }
+}
+
+const compiler = webpack( buildenv === 'production' ? config.production : config.dev );
+
+compiler.run( ( err, stats ) => {
+    if ( err ) {
+        console.log( 'webpack:build', err );
+    }
+    console.log( '[webpack:build]', stats.toString( {
+        chunks: true,
+        colors: true
+    } ) );
+} );
