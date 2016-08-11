8836411c8bdfd6ea327b2bc038b7770494a7c0fd
Chris W. Burke <chris@chriswburke.com>
2016-08-11 19:34:35 -0400

    Sprites task

--- bin/tasks/sprites.js
+++ bin/tasks/sprites.js
@@ -1,17 +1,18 @@
 var sprite = require( 'sprity' );
 sprite.create( {
-    src: '',
-    out: '',
-    style: 'style.scss',
+    src: './src/sprites',
+    out: './build/images/sprites',
+    style: './src/sass/generated/style.scss',
     processor: 'sass',
+    'style-type': 'scss',
     'dimension': [ {
         ratio: 1,
         dpi: 72
     }, {
         ratio: 2,
         dpi: 192
-    } ],
-    'style-type': 'scss'
+    } ]
+
 }, function () {
     console.log( 'done' );
 } );
