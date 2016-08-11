781683ccc4d5d3d03c88bd79c49d80c04d08457b
Chris W. Burke <chris@chriswburke.com>
2016-08-11 19:25:20 -0400

    Adding Build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$1" = 'production' ]; then
+    BUILDENV=production
+else
+    BUILDENV=development
+fi
+
+
 echo -e "\033[1m=> Clean"
 echo -e "-------------------------\033[0m"
 npm run clean
@@ -7,9 +14,19 @@ npm run clean
 echo
 echo -e "\033[1m=> Imagemin"
 echo -e "-------------------------\033[1m"
-npm run images
+npm run images $BUILDENV
+
+echo
+echo -e "\033[1m=> Sass"
+echo -e "-------------------------\033[0m"
+npm run sass $BUILDENV
+
+echo
+echo -e "\033[1m=> Webpack"
+echo -e "-------------------------\033[0m"
+npm run webpack $BUILDENV
 
 echo
 echo -e "\033[1m=> Assemble"
 echo -e "-------------------------\033[0m"
-npm run assemble
+npm run assemble $BUILDENV
