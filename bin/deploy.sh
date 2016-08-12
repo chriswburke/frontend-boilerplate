8ccaa6c0c28a43f9cb504d63574b3fa6337d19a7
Chris W. Burke <chris@chriswburke.com>
2016-08-12 12:11:18 -0400

    Testing the changes to deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
+SERVER=$1
 
-git ls-remote --exit-code $1 >/dev/null 2>&1
-if [ $? -ne 0 ]
-then
-    echo "Unable to deploy!"
-    echo "----------------------------------------------"
-    echo "Server does not exist!"
-    echo "Please add the server and try again."
-	exit 1
-fi
-
-if ! currentbranch=$(git symbolic-ref --short -q HEAD)
-then
+if ! CURRENTBRANCH=$(git symbolic-ref --short -q HEAD); then
 	echo "Cannot find current branch."
 	exit 1
 fi
 
 if ! git diff-index --quiet HEAD --; then
-    echo "Unable to deploy!"
+    echo "Unable to deploy to $SERVER!"
     echo "----------------------------------------------"
 	echo "There are uncommited changes on this repository."
     echo "Please commit your changes and try again."
 	exit 1
 else
-    echo "Starting deployment from branch $currentbranch"
+    echo "Starting deployment from branch $CURRENTBRANCH"
     echo "----------------------------------------------"
-    git push $1 $currentbranch:master
+    git push "$SERVER" "$CURRENTBRANCH":master
 fi
