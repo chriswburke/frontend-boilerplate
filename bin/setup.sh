b8c8b9e29fcfb49cf856be3158484401f525fd3a
Chris W. Burke <chris@chriswburke.com>
2016-08-11 20:01:14 -0400

    Creating loop to add multiple server envs

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,15 +2,20 @@
 
 NAME=$(grep -m1 name package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 
-git ls-remote --exit-code dev >/dev/null 2>&1
-if [ $? -ne 0 ]
-then
-	echo "Remote dev does not exist."
-    echo "Adding dev remote..."
-    git remote add dev dokku@dev.sapientnyc.com:$NAME
-    echo "Dev remote added!"
-    echo "------------------------------------"
-    echo "URL: http://$NAME.dev.sapientnyc.com"
-else
-    echo "Dokku configuration already exists."
-fi
+for i in dev qa staging
+do
+    git ls-remote --exit-code $i >/dev/null 2>&1
+    if [ $? -ne 0 ]
+    then
+    	echo "Remote $i does not exist."
+        echo "Adding $i remote..."
+        git remote add $i dokku@$i.sapientnyc.com:$NAME
+        echo "$i remote added!"
+        echo "------------------------------------"
+        echo "URL: http://$NAME.$i.sapientnyc.com"
+        echo
+        echo
+    else
+        echo "Dokku configuration for $i already exists."
+    fi
+done
