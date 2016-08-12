82fe9f67f8916483ac2b829b0ab94376143427f7
Chris W. Burke <chris@chriswburke.com>
2016-08-12 12:16:59 -0400

    Fixing deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 SERVER=$1
 
+git ls-remote --exit-code ${SERVER} >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Unable to deploy!"
+    echo "----------------------------------------------"
+    echo "Server: $SERVER does not exist!"
+    echo "Please add $SERVER remote and try again."
+	exit 1
+fi
+
 if ! CURRENTBRANCH=$(git symbolic-ref --short -q HEAD); then
 	echo "Cannot find current branch."
 	exit 1
 fi
 
 if ! git diff-index --quiet HEAD --; then
-    echo "Unable to deploy to $SERVER!"
+    echo "Unable to deploy to ${SERVER}!"
     echo "----------------------------------------------"
 	echo "There are uncommited changes on this repository."
     echo "Please commit your changes and try again."
 	exit 1
 else
-    echo "Starting deployment from branch $CURRENTBRANCH"
+    echo "Starting deployment from branch ${CURRENTBRANCH}"
     echo "----------------------------------------------"
-    git push "$SERVER" "$CURRENTBRANCH":master
+    git push ${SERVER} ${CURRENTBRANCH}:master
 fi
